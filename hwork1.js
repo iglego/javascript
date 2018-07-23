3c6b42ad18574b8a2bcc7d7b94d5dc9e211c71b9
iglego <39998858+iglego@users.noreply.github.com>
2018-07-23 14:01:43 +0300

    Add files via upload

---
+++ hwork1.js
@@ -0,0 +1,18 @@
+// Third
+for(i=0;i<=9;i++)
+{
+    for(k=1;k<=9;k++)
+    {
+    console.log(i+'*'+k+'='+i*k+'\n');
+    }
+}
+
+// Fourth
+console.log('\n123\n456\n789');
+
+// Fifth
+let y='*';
+for(m=0;m<=6;m++)
+{
+    console.log(y);
+}
